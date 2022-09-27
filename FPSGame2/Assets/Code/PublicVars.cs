098b88798c71df9344301a472818e4e056badbbe
semsemihere <sh6042@nyu.edu>
2022-09-27 18:21:59 -0400

    first_push
    add UI and start page

---
+++ FPSGame2/Assets/Code/PublicVars.cs
@@ -0,0 +1,34 @@
+// Script for public variables 
+// Score? Bullets, Health bar?, Time 
+// text temporarily stored here
+using TMPro; 
+
+public static class PublicVars
+{
+    static int score = 0; 
+    static int bulletMax = 7; 
+    static int bulletCurr = bulletMax; 
+    static int time = 100; 
+
+    // public TextMeshProUGUI scoreText; 
+
+    // public static void AddScore(int points)
+    // {
+    //     score += 100;
+    // }
+
+    // // reduce the bullet when fired
+    // public static void UseBullet()
+    // {
+    //     bulletCurr -= 1; 
+    // }
+
+    // // reload the bullet 
+    // public static void Reload()
+    // {
+    //     bulletCurr = bulletMax; 
+    // }
+
+
+
+}
