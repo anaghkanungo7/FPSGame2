6397285da67653ef680e277fcc888314aa9aa655
Anagh Kanungo <anaghkanungo7@gmail.com>
2022-09-30 00:14:54 -0400

    Added damage to player

--- FPSGame2/Assets/Code/Stats.cs
+++ FPSGame2/Assets/Code/Stats.cs
@@ -1,31 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Stats : MonoBehaviour
-{
-    private float enemyCount = 0f;
-    private float collectablesFound = 0f;
-    public float collectablesGoal = 10f;
-    public float enemyGoal = 10f;
-    public float playerHealth = 100f;
-    // Start is called before the first frame update
-    void Start()
-    {
+// public class Stats : MonoBehaviour
+// {
+//     private int enemyCount = 0f;
+//     private int collectablesFound = 0f;
+//     public int collectablesGoal = 10f;
+//     public int enemyGoal = 10f;
+
+//     // void Start()
+//     // {
         
-    }
+//     // }
 
-    // Update is called once per frame
-    void Update()
-    {
+//     // // Update is called once per frame
+//     // void Update()
+//     // {
         
-    }
+//     // }
 
-    void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.tag == "Collectable")
-        {
-            collectablesFound++;
-        }
-    }
-}
+//     void OnCollisionEnter(Collision other)
+//     {
+//         if (other.gameObject.tag == "Collectable")
+//         {
+//             collectablesFound++;
+//         }
+//     }
+// }
